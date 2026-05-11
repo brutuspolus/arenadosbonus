8ef1e18445a953da6ecd0fafcbfabb5058d8691f
brutuspolus <brutuspolus@gmail.com>
2026-05-11 18:48:54 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -313,7 +313,7 @@ const sponsorCards = [
   {
     name: 'SpinandFlix 🏋️',
     headerColor: 'gladiator-header-black',
-    logoUrl: 'https://i.imgur.com/L54LO5A.png',
+    logoUrl: 'https://i.imgur.com/ZmhKkNb.png',
     tagline: '200% Bónus até €1000 & 350 Rodadas Grátis',
     perks: [],
     affiliateLink: 'https://spinandflix.goaffnk.com/click?campaign_id=17&pub_id=12',
