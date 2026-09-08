77058adc1be54fa53bd452e36088205745254b53
brutuspolus <brutuspolus@gmail.com>
2026-09-08 23:50:23 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -216,7 +216,7 @@ const sponsorCards = [
   {
     name: 'Windetta Casino',
     headerColor: 'gladiator-header-purple',
-    logoUrl: 'https://i.imgur.com/Jl3SSre.png',
+    logoUrl: 'https://i.imgur.com/bXQ0zOt.png',
     tagline: 'Casino',
     perks: [],
     badgeText: 'Escolhe o teu Bónus',
