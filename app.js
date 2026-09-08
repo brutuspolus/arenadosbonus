9b37a247119cfab70a1022568f454257fd5f6d85
brutuspolus <brutuspolus@gmail.com>
2026-09-08 22:55:43 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -323,7 +323,7 @@ const sponsorCards = [
   {
     name: 'SpinLine',
     headerColor: 'gladiator-header-cream',
-    logoUrl: 'https://i.imgur.com/DSpgRMw.png',
+    logoUrl: 'https://i.imgur.com/aqd2NX4.png',
     tagline: '100% até 300€ + 200 Freespins',
     perks: [],
     badgeText: 'Novo',
