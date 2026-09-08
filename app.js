39f5af68da46ab3083f10989046be4d77b043d6c
brutuspolus <brutuspolus@gmail.com>
2026-09-08 23:18:37 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -177,7 +177,7 @@ const sponsorCards = [
   {
     name: 'STELARIO',
     headerColor: 'gladiator-header-darkblue',
-    logoUrl: 'https://i.imgur.com/GrdJfd0.png',
+    logoUrl: 'https://i.imgur.com/BCoqD0u.png',
     tagline: '100% até 300€ + 100 Freespins',
     perks: [],
     affiliateLink: 'https://record.joinaff.com/_hwHCWKmquIjsP_BZ5nQwFmNd7ZgqdRLk/1/',
